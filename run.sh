d71b9794542d37a814dc7f1044bd774e87ef1f9c
Vadim Ponomarev <vadim@jexia.com>
2020-03-25 14:48:39 +0300

    Parse all available env variables and change it in files

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-FILES=($(find ./__sapper -name "*.js" 2>/dev/null))
-for file in "${FILES[@]}"; do
-  envsubst '$RUNME_API_HOST' < $file > $file.new && mv $file.new $file
-  envsubst '$RUNME_API_SECURE' < $file > $file.new && mv $file.new $file
-  envsubst '$APPLICATION_PROJECT_ID' < $file > $file.new && mv $file.new $file
-  envsubst '$API_KEY' < $file > $file.new && mv $file.new $file
-  envsubst '$API_SECRET' < $file > $file.new && mv $file.new $file
+for file in `find ./__sapper__ -name "*.js"`; do
+  for var in `env | awk -F '=' '{print $1}' | grep -vE '^_.*'`; do
+    echo change variable '$'"$var"'' in file $file
+    envsubst '$'"$var"'' < $file > $file.new && mv $file.new $file
+  done
 done
 
 npm run start
-
