ad0e25153cdabeb01f27a4573fc8b4fec8c99b38
Reinos <info@reinos.nl>
2020-03-26 15:38:28 +0100

    feat: support for Google Analytics

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,11 @@
+<script>
+	import GoogleAnalytics from '../components/UI/GoogleAnalytics.svelte'
+</script>
+
 <style lang="sass" global>
 	@import "../assets/style/global.sass"
 </style>
 
-<slot></slot>
+<GoogleAnalytics />
+
+<slot />
