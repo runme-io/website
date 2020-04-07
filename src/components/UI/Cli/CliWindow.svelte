6291c2e244963792cb065a392cf32af4e69b88fb
Reinos <info@reinos.nl>
2020-04-07 13:52:23 +0200

    refactor(cli-window): to standalone component and inside scrolling

---
+++ src/components/UI/Cli/CliWindow.svelte
@@ -0,0 +1,124 @@
+<script>
+    import CliLoading from './CliLoading.svelte'
+    import PerfectScrollbar from 'perfect-scrollbar'
+    import { afterUpdate } from 'svelte'
+    import { default as AnsiUp } from 'ansi_up'
+
+    export let log = ''
+    export let error
+    export let working = false
+
+    const psContainer = process.browser ? document.querySelector('#perfectScrollbar') : null
+    const ps = process.browser ? new PerfectScrollbar('#perfectScrollbar') : null
+    const ansi_up = new AnsiUp()
+
+    let stickyToBottom = true
+
+    // check if we need to stick to the bottom
+    if (process.browser) {
+        psContainer.addEventListener('ps-scroll-up', () => stickyToBottom = false)
+        psContainer.addEventListener('ps-y-reach-end', () => stickyToBottom = true)
+    }
+
+    $: log = ansi_up.ansi_to_html(log).replace(/[\r\n]/g, "<br />")
+
+    // Append errors to the log
+    $: if (error) {
+      log = `${log}\r\n> Error: ${error}`
+    }
+
+    afterUpdate(() => {
+        ps.update()
+        if (stickyToBottom) {
+            scrollToBottom()
+        }
+    })
+
+    const scrollToTop = () => {
+        stickyToBottom = false
+        psContainer.scrollTop = 0
+    }
+
+    const scrollToBottom = () => {
+        if (process.browser) {
+            psContainer.scrollTop = ps.contentHeight
+            stickyToBottom = true
+        }
+    }
+</script>
+
+<div class="build-log">
+    <header>
+        <ul class="actions">
+            <li class="red"></li>
+            <li class="orange"></li>
+            <li class="green"></li>
+        </ul>
+        <h2>Jexia CLI</h2>
+    </header>
+    <div id="perfectScrollbar" class="content ps">
+        <div class="log">{@html log}</div>
+        {#if working}<CliLoading intervalTimer="100"/>{/if}
+    </div>
+</div>
+
+<style lang="sass">
+    @import '../../../assets/style/theme'
+
+    @keyframes flickerAnimation
+        0%
+            opacity: 1
+        50%
+            opacity: .2
+        100%
+            opacity: 1
+
+    .build-log
+        margin-top: $spacing
+        background-color: #1d1d1d
+        border-radius: .5rem
+
+        header
+            border-top-left-radius: .5rem
+            border-top-right-radius: .5rem
+            background-color: #343434
+            padding: .5rem 2rem
+            display: flex
+            flex-direction: row
+            align-items: baseline
+
+            h2
+                text-align: center
+                color: $white
+                font-size: 1.4rem
+                padding: 0
+                margin: 0
+                flex-grow: 1
+
+            .actions
+                display: inline-block
+
+                li
+                    display: inline-block
+                    width: 1rem
+                    height: 1rem
+                    border-radius: 50%
+
+                    &.green
+                        background-color: #2dc660
+
+                    &.orange
+                        background-color: #fbba60
+
+                    &.red
+                        background-color: #f95e63
+
+        .content
+            color: $white
+            padding: 3rem 2rem
+            word-break: break-word
+            line-height: 2.5rem
+            position: relative
+            min-height: calc(100vh - 25rem)
+            height: calc(100vh - 25rem)
+</style>
--- src/routes/build.svelte
+++ src/routes/build.svelte
@@ -1,29 +1,22 @@
 <script>
     import FixedHeader from '../components/UI/Layout/FixedHeader.svelte'
     import JexiaFooter from '../components/UI/Layout/JexiaFooter.svelte'
-    import { default as AnsiUp } from 'ansi_up';
-    import BuildLoading from '../components/UI/BuildLoading.svelte'
-    import * as animateScroll from 'svelte-scrollto'
     import { goto } from '@sapper/app'
     import { build } from '../components/Stores/Build'
     import { header } from '../components/Stores/Header'
     import { queryParam } from '../components/Helpers/QueryParam'
     import Icon from 'fa-svelte'
-    import { faArrowUp } from '@fortawesome/free-solid-svg-icons/faArrowUp'
-    import { faArrowDown } from '@fortawesome/free-solid-svg-icons/faArrowDown'
     import { isBase64 } from '../components/Helpers/Const'
     import { onDestroy } from 'svelte'
     import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle'
+    import CliWindow from '../components/UI/Cli/CliWindow.svelte'
 
-    const ansi_up = new AnsiUp();
     const buildId = queryParam().get('build_id')
 
     let building = true
     let buildLog = ''
-    let buildSticky = true
     let buildErrorMsg
     let countFrom = 0
-    let displayActionsAsSticky = false
     let dockerRunmeImage = ''
 
     const showBuildError = (error) => {
@@ -34,34 +27,6 @@
     const done = (buildId) => {
         if (process.browser) {
             goto(`/show?build_id=${buildId}`)
-        }
-    }
-
-    const scrollToBottom = () => {
-        if (process.browser) {
-            // calculate the offset height based on the header and the viewport height
-            const headerElement = document.getElementsByTagName('header')
-            const headerHeight = headerElement[0] ? headerElement[0].offsetHeight : 0
-            const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-            const offset = -(vh - (headerHeight - 40)) // should be negative
-
-            // mark as sticky
-            buildSticky = true
-
-            // auto scroll to the last log item
-            animateScroll.scrollTo({ element: '#marker-scroll-to-bottom', offset })
-        }
-    }
-
-    const scrollToTop = () => {
-        buildSticky = false
-        animateScroll.scrollToTop()
-    }
-
-    const getContentHeight = () => {
-        if (process.browser) {
-            const element = document.getElementsByClassName('content')
-            return element[0] ? element[0].offsetHeight : 0
         }
     }
 
@@ -84,7 +49,7 @@
             log.push(deploy_log)
         }
 
-        buildLog = ansi_up.ansi_to_html(log.join('\r\n')).replace(/[\r\n]/g, "<br />")
+        buildLog = log.join('\r\n')
     }
 
     const process = (response) => {
@@ -95,22 +60,12 @@
             showBuildError(`Build failed`)
         }
 
-        // TODO are we sure we need to do this automatically?
-        //  e.g. Netlify does not do that and shows a button to go to the page
         if (status === 'done') {
             done(id)
         }
 
         // collect the log and combine them
         collectLog(build_log, deploy_log)
-
-        // get the height of the content, to determine if we need to set an extra class for the sticky position
-        setTimeout(() => displayActionsAsSticky = getContentHeight() > 300, 200)
-
-        // If needed, we follow the log by scrolling to it
-        if (buildSticky) {
-            setTimeout(() => scrollToBottom(), 200)
-        }
     }
 
     const unsubscribe = build.subscribe(({ error, docker_image, ...response }) => {
@@ -150,29 +105,7 @@
 
     <div class="container">
         <main>
-            <div class="build-log">
-                <header>
-                    <ul class="actions">
-                        <li class="red"></li>
-                        <li class="orange"></li>
-                        <li class="green"></li>
-                    </ul>
-                    <h2>Jexia CLI</h2>
-                </header>
-                <div class="content">
-                    {@html buildLog}
-                    {#if building}<BuildLoading intervalTimer="100"/>{/if}
-                    {#if buildErrorMsg}<div class="not-found">> Error: {buildErrorMsg}</div>{/if}
-
-                    {#if building && buildLog}
-                        <div class="actions" class:sticky={displayActionsAsSticky}>
-                            <button title="Scroll to bottom" class:active={buildSticky} on:click={scrollToBottom}><Icon icon="{faArrowDown}"/></button>
-                            <button title="Scroll to top" on:click={scrollToTop}><Icon icon="{faArrowUp}"/></button>
-                        </div>
-                        <div id="marker-scroll-to-bottom"></div>
-                    {/if}
-                </div>
-            </div>
+            <CliWindow working={building} error={buildErrorMsg} log={buildLog}/>
 
             {#if dockerRunmeImage}
                 <div class="docker-image">
@@ -186,93 +119,6 @@
 </div>
 
 <style lang="sass">
-    @import '../assets/style/theme'
-
-    @keyframes flickerAnimation
-        0%
-            opacity: 1
-        50%
-            opacity: .2
-        100%
-            opacity: 1
-
-    .build-log
-        margin-top: $spacing
-        background-color: #1d1d1d
-        border-radius: .5rem
-
-        header
-            border-top-left-radius: .5rem
-            border-top-right-radius: .5rem
-            background-color: #343434
-            padding: .5rem 2rem
-            display: flex
-            flex-direction: row
-            align-items: baseline
-
-            h2
-                text-align: center
-                color: $white
-                font-size: 1.4rem
-                padding: 0
-                margin: 0
-                flex-grow: 1
-
-            .actions
-                display: inline-block
-
-                li
-                    display: inline-block
-                    width: 1rem
-                    height: 1rem
-                    border-radius: 50%
-
-                    &.green
-                        background-color: #2dc660
-
-                    &.orange
-                        background-color: #fbba60
-
-                    &.red
-                        background-color: #f95e63
-
-        .content
-            color: $white
-            padding: 3rem 2rem
-            min-height: 30rem
-            word-break: break-word
-            line-height: 2.5rem
-            position: relative
-
-            .actions
-                position: absolute
-                bottom: 1.5rem
-                right: 1.5rem
-                text-align: right
-                display: flex
-                flex-direction: row
-                margin-left: auto
-                width: 7rem
-                align-items: baseline
-                background: #343434
-                border-radius: 2rem
-                padding: 1rem
-
-                &.sticky
-                    position: sticky
-
-                > button
-                    display: inline-flex
-                    padding: 0 .5rem
-                    cursor: pointer
-                    border: 0
-                    background: transparent
-                    color: $white
-                    outline: none
-
-                    &.active
-                        animation: flickerAnimation 1s infinite
-
     .docker-image
         font-size: 1.2rem
         margin-top: 2rem
