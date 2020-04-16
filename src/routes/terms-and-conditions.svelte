81faaa6f0b8dd7ff265312bd26c58a6e4ae18d62
Reinos <info@reinos.nl>
2020-04-16 09:50:57 +0200

    feat: add terms and conditions page

---
+++ src/routes/terms-and-conditions.svelte
@@ -0,0 +1,10 @@
+<script>
+    import MainLayout from '../components/UI/Layout/MainLayout.svelte'
+</script>
+
+<MainLayout title="Terms and conditions">
+    <div class="main-content">
+        <h1>Terms and conditions</h1>
+        <p></p>
+    </div>
+</MainLayout>
