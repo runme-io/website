022b4e05f0d1fd14c9f0820cc0658625bcde379b
Reinos <info@reinos.nl>
2020-04-16 12:27:43 +0200

    fix(run): update rate limit message

--- src/routes/run.svelte
+++ src/routes/run.svelte
@@ -8,34 +8,37 @@
     import FixedHeader from '../components/UI/Layout/FixedHeader.svelte'
     import ContentLayout from '../components/UI/Layout/ContentLayout.svelte'
     import LoadingBlock from '../components/UI/Loader/LoadingBlock.svelte'
+    import CountDown from '../components/UI/Counter/CountDown.svelte'
 
     let timeToRestart
     let showCountDown = false
     let showCountDownBlock = false
     let loading = true
-    let errorMsg = ''
+    let hasError = false
+    let generalError = false
+    let rateLimitError = false
     let showDefaultErrorMsg = true
+
+    $: hasError = generalError || rateLimitError
 
     const app_id = queryParam().get('app_id')
 
     const unsubHeader = header.subscribe(({ countDownFinish }) => {
-      if (countDownFinish) {
-          location.reload()
-      }
+        if (countDownFinish) {
+            location.reload()
+        }
     })
 
     const unsubBuild = build.subscribe(({ build_id, error }) => {
-        if (error) {
+        if (error && error.nextBuild) {
             loading = false
-            errorMsg = error.message
-
-            if (error.nextBuild) {
-                errorMsg = 'There are to many build triggerd after each other. Please wait until we can run a new build for you'
-                showDefaultErrorMsg = false
-                header.showCountDown(error.nextBuild, 'Next build in')
-            }
+            rateLimitError = true
+            showDefaultErrorMsg = false
+            header.showCountDown(error.nextBuild, 'Next build in')
         } else if (error) {
-            errorMsg = `No app has been found with ID "${app_id}". Please (re)generate a new button <a href="/">here</a> in order to run your application.`
+            loading = false
+            generalError = true
+
         } else if (build_id) {
             if (process.browser) {
                 goto(`/build?build_id=${build_id}`)
@@ -67,10 +70,17 @@
         <LoadingBlock>
             You application is firing up, please hang with us so we can collect your repo info and start building your application.
         </LoadingBlock>
-    {:else if errorMsg}
-        <h1>Error</h1>
+    {:else if hasError}
+        <h1>Oeps!</h1>
         <p>
-            {@html errorMsg}.
+            {#if rateLimitError}
+                There are to many build triggers for this particular repo. Please hang on, Runme will try to run it in a new build, after <CountDown /> minutes.
+            {/if}
+
+            {#if generalError}
+                No app has been found with ID "{app_id}. Please (re)generate a new button <a href="/">here</a> in order to run your application.
+            {/if}
+
             {#if showDefaultErrorMsg}Please go back to the <a href="/">generator</a> page and create a button and run url.{/if}
         </p>
 
