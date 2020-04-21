333d2e61e9823c44238ef84781d9b8a6c7b6e2c0
Reinos <info@reinos.nl>
2020-04-21 12:32:08 +0200

    feat(menu): add discord link

---
+++ src/Data/menu.js
@@ -0,0 +1,22 @@
+export const menuItems = [
+  {
+    label: 'Generate',
+    link: '/',
+    newWindow: false,
+  },
+  {
+    label: 'How it works',
+    link: '/how-it-works',
+    newWindow: false,
+  },
+  {
+    label: 'About',
+    link: '/about',
+    newWindow: false,
+  },
+  {
+    label: 'Discord',
+    link: 'https://discord.gg/3ZYKHu',
+    newWindow: true,
+  },
+]
