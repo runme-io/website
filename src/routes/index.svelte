8078c580c8abdb543c161d5f02f243105de687b0
Marlos Borges <marlos.borges@gmail.com>
2020-04-28 17:38:59 +0200

    refactor(generate-form): extract form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,187 +1,44 @@
 <script>
-	import RunmeButton from '../components/Runme/RunmeButton.svelte'
-	import OptionEnvVars from '../components/Runme/Generator/OptionEnvVars.svelte'
-	import { application } from './../components/Stores/Application'
-	import MainLayout from '../components/UI/Layout/MainLayout.svelte'
-	import Icon from 'fa-svelte'
-	import { faSortDown } from '@fortawesome/free-solid-svg-icons/faSortDown'
-	import { faSortUp } from '@fortawesome/free-solid-svg-icons/faSortUp'
-	import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle'
-	import GenerateButton from '../components/Runme/Generator/GenerateButton.svelte'
-	import TextInput from '../components/UI/TextInput.svelte'
-	import { isDockerUrl, isEmpty, isGitUrl, queryParam, parseGitUrl, setUrl } from '../Helpers'
-	import Alert from '../components/UI/Alert.svelte'
-	import GithubReadme from '../components/UI/GitHub/GithubReadme.svelte'
 	import { onDestroy } from 'svelte'
+	import Icon from 'fa-svelte'
+	import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+	import RunmeButton from '../components/Runme/RunmeButton.svelte'
+	import GenerateForm from '../components/Runme/GenerateForm.svelte'
+	import MainLayout from '../components/UI/Layout/MainLayout.svelte'
+	import TextInput from '../components/UI/TextInput.svelte'
+	import GithubReadme from '../components/UI/GitHub/GithubReadme.svelte'
+	import { setUrl } from '../Helpers'
 
 	// form fields
 	let embedStyle = 'markdown'
-	let repoUrl = ''
-	let repoUrlParsed = ''
-	let repoBranch = 'master'
-	let dockerImage = ''
-	let envVars = {}
-
-	// form states
-	let repoUrlValid
-	let repoBranchValid
-	let dockerImageValid
-	let formIsValid
 
 	// others
-	let showAdvancedOptions = false
 	let embedCode = ''
-	let dropDownIcon = faSortDown
-	let canShowEmbed = false
-	let loading = false
-	let buttonText = 'Generate'
-	let errorMsg = ''
-	let appId = ''
-	let errorType = 'warning'
-	let envVarsValid = true // true by default as this is optional
 	let showRunmeFooter = false
+	$: canShowEmbed = embedCode
 
-	const exclamationIcon = faExclamationCircle
-
-	// ensure correct GIT url
-	$: parseGitUrl(repoUrl).then(url => repoUrlParsed = url)
-
-	// check if the fields are valid
-	$: repoUrlValid = !isEmpty(repoUrlParsed) && isGitUrl(repoUrlParsed)
-	$: repoBranchValid = repoBranch !== ''
-	$: dockerImageValid = dockerImage === '' || isDockerUrl(dockerImage)
-	$: formIsValid = repoUrlValid && dockerImageValid && envVarsValid
-
-	const setError = (error, type = 'warning') => {
-		errorType = type
-		errorMsg = error
-	}
-
-	const clearError = () => {
-		queryParam().clear('error')
-		setError('')
-	}
-
-	// check if we have to deal with an error from a previous page
-	const hasError = queryParam().get('error')
-	if (hasError) {
-		setError(hasError, 'danger')
-	}
-
-	const isLoading = (status) => {
-		if (status) {
-			loading = true
-			formIsValid = false
-			buttonText = 'Generating...'
-		} else {
-			loading = false
-			formIsValid = repoUrlValid
-			buttonText = 'Generate'
-		}
-	}
-
-	const unsubscribe = application.subscribe(({ id, error }) => {
-		if (error) {
-			console.log(error)
-			setError('There is a problem with creating your button. Please try again later')
-			isLoading(false)
-		}
-
-		if (id) {
-			appId = id
-			isLoading(false)
-			showEmbedCode()
-		}
-	})
-
-	function toggleAdvanced () {
-		showAdvancedOptions = !showAdvancedOptions
-		dropDownIcon = showAdvancedOptions ? faSortDown : faSortUp
-	}
-
-	function generateEmbedCode () {
-		// set the button to loading
-		isLoading(true)
-
-		// clear previous errors
-		clearError()
-
-		// create an application and assign it to the store
-		application.create(repoUrlParsed, repoBranch, dockerImage, envVars)
-	}
-
-	function showEmbedCode () {
-		// Global statement: we need this var evaluation also in our template
-		canShowEmbed = repoUrl !== '' && repoBranch !== '' && appId !== ''
-
+	function showEmbedCode (appId) {
 		// show the footer
 		showRunmeFooter = true
 
 		const runUrl = setUrl(`run?app_id=${appId}`)
 
-		if (canShowEmbed && embedStyle === 'markdown') {
+		if (embedStyle === 'markdown') {
 			embedCode = `[![Runme](https://svc.runme.io/static/button.svg)](${runUrl})`
-		} else if (canShowEmbed && embedStyle === 'reStructuredText') {
+		} else if (embedStyle === 'reStructuredText') {
 			embedCode = `.. image:: https://svc.runme.io/static/button.svg\n    :target: ${runUrl}`
 		}
 	}
-
-	onDestroy(unsubscribe)
 </script>
 
 <MainLayout {showRunmeFooter} showTechnologyIcons={true} title="Run your application from any public Git-repo with one click">
 	<div class="generator-page">
 		<h1>Generate <RunmeButton/>  button for your repo.</h1>
 		<section class="generator">
-			{#if errorMsg}<Alert type={errorType}>{errorMsg}</Alert>{/if}
-
-			<div id="repo-url">
-				<TextInput
-					id="repo-url"
-					label="Copy your repo URL below."
-					valid={repoUrlValid}
-					validityMessage="Please enter a valid Repository url."
-					value={repoUrl}
-					placeholder="https://github.com/jexia/test-node-app.git"
-					on:enter={generateEmbedCode}
-					on:input={event => (repoUrl = event.target.value)}
-				/>
-			</div>
-
-			<div id="advanced-options">
-				<div class="advanced-option" on:click={toggleAdvanced}>Advanced options <Icon icon={dropDownIcon}/></div>
-				{#if showAdvancedOptions}
-					<div class="spacing-top">
-						<TextInput
-							id="repo-branch"
-							label="What branch should we use?"
-							valid={repoBranchValid}
-							validityMessage="Please enter a valid branch."
-							value={repoBranch}
-							placeholder="master"
-							on:input={event => (repoBranch = event.target.value)}
-						/>
-						<TextInput
-							id="docker-image"
-							label="Copy your docker image in format <image>:<tag> (optional)"
-							valid={dockerImageValid}
-							validityMessage="Please enter a valid Docker image url."
-							value={dockerImage}
-							placeholder="<image>:<tag>"
-							on:enter={generateEmbedCode}
-							on:input={event => (dockerImage = event.target.value)}
-						/>
-						<OptionEnvVars on:items={event => envVars = event.detail} on:valid={event => envVarsValid = event.detail} />
-					</div>
-				{/if}
-			</div>
-
-			<div class="generate-button">
-				<GenerateButton {loading} disabled={!formIsValid} on:click={generateEmbedCode}>{buttonText}</GenerateButton>
-			</div>
+			<GenerateForm on:generate={event => showEmbedCode(event.detail)} />
 
 			<div class="requirements">
-				<h3><div class="requirements__icon"><Icon icon={exclamationIcon} /></div> Requirements</h3>
+				<h3><div class="requirements__icon"><Icon icon={faExclamationCircle} /></div> Requirements</h3>
 				<p>In order to run your application properly, there should be at least one requirement applied to your repository. See <a href="/how-it-works">this</a> page for more information.</p>
 				<ul>
 					<li>your repo has a Dockerfile in the root directory</li>
@@ -225,19 +82,6 @@
 
 	.generator
 		padding-top: $spacing
-
-	.spacing-top
-		padding-top: 2rem
-
-	.advanced-option
-		font-size: 1.2rem
-		cursor: pointer
-
-	.advanced-option :global(.fa-svelte)
-		width: 1rem
-
-	.generate-button
-		text-align: center
 
 	.embed-result
 		@include dashed-line(top)
