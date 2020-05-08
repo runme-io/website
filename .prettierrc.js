fb61e3df981187af9482fa0c7102bfb831234ef3
Marlos Borges <marlos.borges@gmail.com>
2020-05-08 17:19:47 +0200

    build: add config files for eslint/prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  svelteSortOrder : 'styles-scripts-markup',
+  svelteStrictMode: true,
+  svelteBracketNewLine: true,
+  plugins: ['prettier-plugin-svelte'],
+}
