04bdcf758828e62fbd0e46624d712352b1199de4
Marlos Borges <marlos.borges@gmail.com>
2020-05-12 15:15:29 +0200

    style: lint files

--- src/components/Runme/GenerateSpecForm.svelte
+++ src/components/Runme/GenerateSpecForm.svelte
@@ -25,13 +25,12 @@
     ? value.command.length && !value.command.find(isEmpty)
     : !isEmpty(value.command)
 
-  $: dispatch(
-    'validate',
-    envVarsValid &&
-    isBuildCommandValid &&
-    isDockerImageValid &&
-    isPortValid &&
-    isCommandValid,
+  $: dispatch('validate',
+    envVarsValid
+    && isBuildCommandValid
+    && isDockerImageValid
+    && isPortValid
+    && isCommandValid,
   )
 
   $: {
@@ -79,54 +78,48 @@
 
 <div>
   {#if isApp}
-      <div class="switch">
-        <label>Do you have a built docker image?</label>
-        <InputSwitch
-          showLabels={true}
-          value={value.hasDockerImage}
-          on:checked={({ detail }) => {
-            value.hasDockerImage = detail
-          }}
-        />
-      </div>
+    <div class="switch">
+      <label>Do you have a built docker image?</label>
+      <InputSwitch
+        showLabels={true}
+        value={value.hasDockerImage}
+        on:checked={({ detail }) => (value.hasDockerImage = detail)}
+      />
+    </div>
 
-      {#if value.hasDockerImage}
-        <TextInput
-          id="docker-image"
-          label="Copy your docker image in format <image>:<tag> (optional)"
-          valid={isDockerImageValid}
-          validityMessage="Please enter a valid Docker image url."
-          value={value.dockerImage}
-          placeholder="<image>:<tag>"
-          on:input={({ target }) => {
-            value.dockerImage = target.value
-          }}
-        />
-      {/if}
+    {#if value.hasDockerImage}
+      <TextInput
+        id="docker-image"
+        label="Copy your docker image in format <image>:<tag> (optional)"
+        valid={isDockerImageValid}
+        validityMessage="Please enter a valid Docker image url."
+        value={value.dockerImage}
+        placeholder="<image>:<tag>"
+        on:input={({ target }) => (value.dockerImage = target.value)}
+      />
+    {/if}
   {/if}
 
   {#if !value.hasDockerImage}
-      <DockerImageSelect
-        valid={isDockerImageValid}
-        validityMessage="A valid docker image is required"
-        value={value.dockerImage}
-        {sourceType}
-        on:change={({ detail }) => setDockerImageValue(detail)}
-      />
+    <DockerImageSelect
+      valid={isDockerImageValid}
+      validityMessage="A valid docker image is required"
+      value={value.dockerImage}
+      {sourceType}
+      on:change={({ detail }) => setDockerImageValue(detail)}
+    />
 
-      {#if isApp}
-        <TextInput
-          id="build_command"
-          label={`Which command should we use to build your ${sourceTypeLabel}?`}
-          valid={isBuildCommandValid}
-          validityMessage="Build command is required"
-          placeholder="npm run build"
-          value={value.build_command}
-          on:input={({ target }) => {
-            value.build_command = target.value
-          }}
-        />
-      {/if}
+    {#if isApp}
+      <TextInput
+        id="build_command"
+        label={`Which command should we use to build your ${sourceTypeLabel}?`}
+        valid={isBuildCommandValid}
+        validityMessage="Build command is required"
+        placeholder="npm run build"
+        value={value.build_command}
+        on:input={({ target }) => (value.build_command = target.value)}
+      />
+    {/if}
   {/if}
 
   {#if isApp}
@@ -139,9 +132,7 @@
       type="number"
       min="0"
       value={value.port}
-      on:input={({ target }) => {
-        value.port = target.value
-      }}
+      on:input={({ target }) => (value.port = target.value)}
     />
   {/if}
 
@@ -153,9 +144,7 @@
     validityMessage="Commands are required"
     placeholder="npm run start"
     value={value.command}
-    on:change={({ detail }) => {
-      value.command = detail
-    }}
+    on:change={({ detail }) => (value.command = detail)}
   />
 
   <OptionEnvVars
@@ -163,8 +152,6 @@
     on:items={({ detail }) => {
       value.envVars = detail
     }}
-    on:valid={({ detail }) => {
-      envVarsValid = detail
-    }}
+    on:valid={({ detail }) => (envVarsValid = detail)}
   />
 </div>
