0329f193b36b70e69bbfcff878cd04482d71d3b2
Marlos Borges <marlos.borges@gmail.com>
2020-05-12 15:15:29 +0200

    style: lint files

--- src/components/Runme/GenerateSpecForm.svelte
+++ src/components/Runme/GenerateSpecForm.svelte
@@ -1,155 +1,170 @@
 <script>
-    import { createEventDispatcher } from 'svelte'
-    import OptionEnvVars from './Generator/OptionEnvVars.svelte'
-    import TextInput from '../UI/TextInput.svelte'
-    import MultipleTextInput from '../UI/MultipleTextInput.svelte'
-    import InputSwitch from '../UI/InputSwitch.svelte'
-    import DockerImageSelect from '../UI/DockerImageSelect.svelte'
-    import { isDockerUrl, isEmpty } from '../../Helpers'
-    import { DOCKER_SELECT_LANGUAGE, DOCKER_SELECT_SERVICES } from '../../Consts'
+  import { createEventDispatcher } from 'svelte'
+  import OptionEnvVars from './Generator/OptionEnvVars.svelte'
+  import TextInput from '../UI/TextInput.svelte'
+  import MultipleTextInput from '../UI/MultipleTextInput.svelte'
+  import InputSwitch from '../UI/InputSwitch.svelte'
+  import DockerImageSelect from '../UI/DockerImageSelect.svelte'
+  import { isDockerUrl, isEmpty } from '../../Helpers'
+  import { DOCKER_SELECT_LANGUAGE } from '../../Consts'
 
-    const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher()
 
-    export let value = {}
-    // The source type of the docker image select
-    export let sourceType
+  export let value = {}
+  // The source type of the docker image select
+  export let sourceType
 
-    let isApp
+  let isApp
 
-    // form states
-    let envVarsValid = true // true by default as this is optional
-    $: isDockerImageValid = isDockerUrl(value.dockerImage)
-    $: isPortValid = !isApp || !isEmpty(value.port)
-    $: isBuildCommandValid = !isApp || value.hasDockerImage || !isEmpty(value.build_command)
-    $: isCommandValid = Array.isArray(value.command)
-        ? value.command.length && !value.command.find(isEmpty)
-        : !isEmpty(value.command)
+  // form states
+  let envVarsValid = true // true by default as this is optional
+  $: isDockerImageValid = isDockerUrl(value.dockerImage)
+  $: isPortValid = !isApp || !isEmpty(value.port)
+  $: isBuildCommandValid = !isApp || value.hasDockerImage || !isEmpty(value.build_command)
+  $: isCommandValid = Array.isArray(value.command)
+    ? value.command.length && !value.command.find(isEmpty)
+    : !isEmpty(value.command)
 
-    $: dispatch('validate',
-        envVarsValid
-        && isBuildCommandValid
-        && isDockerImageValid
-        && isPortValid
-        && isCommandValid
-    )
+  $: dispatch(
+    'validate',
+    envVarsValid &&
+    isBuildCommandValid &&
+    isDockerImageValid &&
+    isPortValid &&
+    isCommandValid,
+  )
 
-    $: {
-        if (value.hasDockerImage) {
-            value.build_command = '' // clear command when user has docker image
-        }
+  $: {
+    if (value.hasDockerImage) {
+      value.build_command = '' // clear command when user has docker image
+    }
+  }
+
+  $: {
+    isApp = sourceType === DOCKER_SELECT_LANGUAGE
+    if (!isApp) {
+      value.hasDockerImage = false
+    }
+  }
+  $: sourceTypeLabel = isApp ? 'application' : 'service'
+
+  function setDockerImageValue (dockerImageValue) {
+    if (!dockerImageValue) {
+      value.dockerImage = ''
+      return
     }
 
-    $: {
-        isApp = sourceType === DOCKER_SELECT_LANGUAGE
-        if (!isApp) {
-            value.hasDockerImage = false
-        }
+    const { image, tag, port } = dockerImageValue
+
+    if (port) {
+      value.port = port
     }
-    $: sourceTypeLabel = isApp ? 'application' : 'service'
 
-    function setDockerImageValue (dockerImageValue) {
-        if (!dockerImageValue) {
-            value.dockerImage = ''
-            return
-        }
-
-        const { image, tag, port } = dockerImageValue
-
-        if (port) {
-            value.port = port
-        }
-
-        value.dockerImage = `${image}:${tag}`
-    }
+    value.dockerImage = `${image}:${tag}`
+  }
 </script>
 
 <style lang="sass">
-    @import './assets/style/theme'
+  @import './assets/style/theme'
 
-    .switch
-        align-items: center
-        display: flex
-        font-size: $label-font-size
-        margin-bottom: $form-control-margin
+  .switch
+    align-items: center
+    display: flex
+    font-size: $label-font-size
+    margin-bottom: $form-control-margin
 
-        label
-            margin-right: $label-margin
+    label
+      margin-right: $label-margin
 </style>
 
 <div>
-    {#if isApp}
-        <div class="switch">
-            <label>Do you have a built docker image?</label>
-            <InputSwitch
-                showLabels={true}
-                value={value.hasDockerImage}
-                on:checked={({ detail }) => value.hasDockerImage = detail}
-            />
-        </div>
-
-        {#if value.hasDockerImage}
-            <TextInput
-                id="docker-image"
-                label="Copy your docker image in format <image>:<tag> (optional)"
-                valid={isDockerImageValid}
-                validityMessage="Please enter a valid Docker image url."
-                value={value.dockerImage}
-                placeholder="<image>:<tag>"
-                on:input={({ target }) => value.dockerImage = target.value}
-            />
-        {/if}
-    {/if}
-
-    {#if !value.hasDockerImage}
-        <DockerImageSelect
-            valid={isDockerImageValid}
-            validityMessage="A valid docker image is required"
-            value={value.dockerImage}
-            {sourceType}
-            on:change={({ detail }) => setDockerImageValue(detail)}
+  {#if isApp}
+      <div class="switch">
+        <label>Do you have a built docker image?</label>
+        <InputSwitch
+          showLabels={true}
+          value={value.hasDockerImage}
+          on:checked={({ detail }) => {
+            value.hasDockerImage = detail
+          }}
         />
+      </div>
 
-        {#if isApp}
-            <TextInput
-                id="build_command"
-                label={`Which command should we use to build your ${sourceTypeLabel}?`}
-                valid={isBuildCommandValid}
-                validityMessage="Build command is required"
-                placeholder="npm run build"
-                value={value.build_command}
-                on:input={({ target }) => value.build_command = target.value}
-            />
-        {/if}
-    {/if}
-
-    {#if isApp}
+      {#if value.hasDockerImage}
         <TextInput
-            id="port"
-            label={`Which port does your ${sourceTypeLabel} expose?`}
-            valid={isPortValid}
-            validityMessage="Port is required"
-            placeholder="8080"
-            type="number"
-            min="0"
-            value={value.port}
-            on:input={({ target }) => value.port = target.value}
+          id="docker-image"
+          label="Copy your docker image in format <image>:<tag> (optional)"
+          valid={isDockerImageValid}
+          validityMessage="Please enter a valid Docker image url."
+          value={value.dockerImage}
+          placeholder="<image>:<tag>"
+          on:input={({ target }) => {
+            value.dockerImage = target.value
+          }}
         />
-    {/if}
+      {/if}
+  {/if}
 
-    <MultipleTextInput
-        id="commands"
-        addLabel="Add more commands"
-        label={`Which commands should we use to run your ${sourceTypeLabel}?`}
-        valid={isCommandValid}
-        validityMessage="Commands are required"
-        placeholder="npm run start"
-        value={value.command}
-        on:change={({ detail }) => value.command = detail}
-    />
+  {#if !value.hasDockerImage}
+      <DockerImageSelect
+        valid={isDockerImageValid}
+        validityMessage="A valid docker image is required"
+        value={value.dockerImage}
+        {sourceType}
+        on:change={({ detail }) => setDockerImageValue(detail)}
+      />
 
-    <OptionEnvVars
-        value={value.envVars}
-        on:items={({ detail }) => value.envVars = detail}
-        on:valid={event => envVarsValid = event.detail}
+      {#if isApp}
+        <TextInput
+          id="build_command"
+          label={`Which command should we use to build your ${sourceTypeLabel}?`}
+          valid={isBuildCommandValid}
+          validityMessage="Build command is required"
+          placeholder="npm run build"
+          value={value.build_command}
+          on:input={({ target }) => {
+            value.build_command = target.value
+          }}
+        />
+      {/if}
+  {/if}
+
+  {#if isApp}
+    <TextInput
+      id="port"
+      label={`Which port does your ${sourceTypeLabel} expose?`}
+      valid={isPortValid}
+      validityMessage="Port is required"
+      placeholder="8080"
+      type="number"
+      min="0"
+      value={value.port}
+      on:input={({ target }) => {
+        value.port = target.value
+      }}
     />
+  {/if}
+
+  <MultipleTextInput
+    id="commands"
+    addLabel="Add more commands"
+    label={`Which commands should we use to run your ${sourceTypeLabel}?`}
+    valid={isCommandValid}
+    validityMessage="Commands are required"
+    placeholder="npm run start"
+    value={value.command}
+    on:change={({ detail }) => {
+      value.command = detail
+    }}
+  />
+
+  <OptionEnvVars
+    value={value.envVars}
+    on:items={({ detail }) => {
+      value.envVars = detail
+    }}
+    on:valid={({ detail }) => {
+      envVarsValid = detail
+    }}
+  />
 </div>
--- src/components/UI/DockerImageSelect.svelte
+++ src/components/UI/DockerImageSelect.svelte
@@ -1,77 +1,81 @@
 <script>
-    import { createEventDispatcher, onMount } from 'svelte'
-    import DockerImages from '../Stores/DockerImages'
+  import { createEventDispatcher, onMount } from 'svelte'
+  import DockerImages from '../Stores/DockerImages'
 
-    const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher()
 
-    /*
-     * type of the docker image source,
-     * available values:
-     * - DOCKER_SELECT_LANGUAGE
-     * - DOCKER_SELECT_SERVICES
-     */
-    export let sourceType
-    // the object preloaded of docker images
-    export let value = null
-    export let valid = true
-    export let validityMessage = ''
+  /*
+   * type of the docker image source,
+   * available values:
+   * - DOCKER_SELECT_LANGUAGE
+   * - DOCKER_SELECT_SERVICES
+   */
+  export let sourceType
+  // the object preloaded of docker images
+  export let value = null
+  export let valid = true
+  export let validityMessage = ''
 
-    let imageItem = null
-    let tag = ''
-    let touched = false
+  let imageItem = null
+  let tag = ''
+  let touched = false
 
-    $: items = $DockerImages[sourceType.key] || []
-    $: hasValidSelection = imageItem && tag
-    $: disabled = !imageItem
-    $: tags = imageItem ? imageItem.tags : []
-    $: invalid = !valid && touched
+  $: items = $DockerImages[sourceType.key] || []
+  $: hasValidSelection = imageItem && tag
+  $: disabled = !imageItem
+  $: tags = imageItem ? imageItem.tags : []
+  $: invalid = !valid && touched
 
-    $: {
-        dispatch('change', hasValidSelection ? getValue() : null)
+  $: {
+    dispatch('change', hasValidSelection ? getValue() : null)
+  }
+
+  function getValue () {
+    const { image, container_port: port } = imageItem
+
+    return {
+      image,
+      tag,
+      port,
     }
+  }
 
-    function getValue () {
-        const { image, container_port: port } = imageItem
-
-        return {
-            image,
-            tag,
-            port,
-        }
-    }
-
-    onMount(() => {
-        const [imageName, tagName] = (value || '').split(':')
-        imageItem = items.find(({ image }) => image === imageName)
-        tag = tagName
-    })
+  onMount(() => {
+    const [imageName, tagName] = (value || '').split(':')
+    imageItem = items.find(({ image }) => image === imageName)
+    tag = tagName
+  })
 </script>
 
 <div class="form-control">
-    <label for="docker-image">{sourceType.label}</label>
-    <select
-        class:invalid={invalid}
-        id="docker-image"
-        bind:value={imageItem}
-        on:change={() => tag = ''}
-    >
-        <option value="">Select a docker image</option>
-        {#each items as item}
-            <option value={item}>{item.name}</option>
-        {/each}
-    </select>
-    <span>:</span>
-    <select
-        {disabled}
-        bind:value={tag}
-        on:blur={() => touched = true}
-    >
-        <option value="">Select a tag</option>
-        {#each tags as tagName}
-            <option>{tagName}</option>
-        {/each}
-    </select>
-    {#if validityMessage && invalid}
-        <p class="error-message">{validityMessage}</p>
-    {/if}
+  <label for="docker-image">{sourceType.label}</label>
+  <select
+    class:invalid={invalid}
+    id="docker-image"
+    bind:value={imageItem}
+    on:change={() => {
+      tag = '' // reset tag value
+    }}
+  >
+    <option value="">Select a docker image</option>
+    {#each items as item}
+      <option value={item}>{item.name}</option>
+    {/each}
+  </select>
+  <span>:</span>
+  <select
+    {disabled}
+    bind:value={tag}
+    on:blur={() => {
+      touched = true
+    }}
+  >
+    <option value="">Select a tag</option>
+    {#each tags as tagName}
+      <option>{tagName}</option>
+    {/each}
+  </select>
+  {#if validityMessage && invalid}
+    <p class="error-message">{validityMessage}</p>
+  {/if}
 </div>
