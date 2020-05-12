7154734b451d48bfc75e2bf22f13017621e085d4
Marlos Borges <marlos.borges@gmail.com>
2020-05-12 17:06:26 +0200

    style: lint files

--- src/Helpers/SpecGenerator.js
+++ src/Helpers/SpecGenerator.js
@@ -1,82 +1,82 @@
 function parseSpecService (service, isMain = false) {
-    let parsedService = {
-        command: service.command,
-    }
+  let parsedService = {
+    command: service.command,
+  }
 
-    if (Object.values(service.envVars || {}).length) {
-        parsedService = {
-            ...parsedService,
-            environment: service.envVars,
-        }
+  if (Object.values(service.envVars || {}).length) {
+    parsedService = {
+      ...parsedService,
+      environment: service.envVars,
     }
+  }
 
-    if (isMain && !service.hasDockerImage) {
-        parsedService = {
-            ...parsedService,
-            build: {
-                type: 'dockerfile',
-                config: './.runme/Dockerfile'
-            },
-        }
-    } else {
-        parsedService = {
-            ...parsedService,
-            image: service.dockerImage,
-        }
+  if (isMain && !service.hasDockerImage) {
+    parsedService = {
+      ...parsedService,
+      build: {
+        type: 'dockerfile',
+        config: './.runme/Dockerfile',
+      },
     }
+  } else {
+    parsedService = {
+      ...parsedService,
+      image: service.dockerImage,
+    }
+  }
 
-    if (!service.port) {
-        return parsedService
-    }
+  if (!service.port) {
+    return parsedService
+  }
 
-    return {
-        ...parsedService,
-        ports: [
-            {
-                container: parseInt(service.port),
-                public: 80
-            },
-        ],
-    }
+  return {
+    ...parsedService,
+    ports: [
+      {
+        container: parseInt(service.port),
+        public: 80,
+      },
+    ],
+  }
 }
 
 async function generateYaml (serviceList) {
-    const { default: YAML } = await import('yaml')
+  const { default: YAML } = await import('yaml')
 
-    const services = serviceList.slice(1)
-        .map(parseSpecService)
-        .reduce(
-            (accumulator, service, index) => {
-                accumulator[`service_${index + 1}`] = service
-                return accumulator
-            },
-            {
-                app: parseSpecService(serviceList[0], true),
-            },
-        )
+  const services = serviceList.slice(1)
+    .map(parseSpecService)
+    .reduce(
+      (accumulator, service, index) => {
+        accumulator[`service_${index + 1}`] = service
+        return accumulator
+      },
+      {
+        app: parseSpecService(serviceList[0], true),
+      },
+    )
 
-    return 'version: 1.0\n' + YAML.stringify({
-        publish: 'app',
-        services,
-    })
+  return 'version: 1.0\n' + YAML.stringify({
+    publish: 'app',
+    services,
+  })
 }
 
 function generateDockerfile (serviceList) {
-    const appValue = serviceList[0] || {}
+  const appValue = serviceList[0] || {}
 
-    if (appValue.hasDockerImage) {
-        return ''
-    }
+  if (appValue.hasDockerImage) {
+    return ''
+  }
 
-    return `FROM ${appValue.dockerImage}
+  return `FROM ${appValue.dockerImage}
 WORKDIR /app
 COPY . .
 RUN ${appValue.build_command}`
 }
 
 export async function generateSpec (serviceList) {
-    return {
-        yaml: await generateYaml(serviceList),
-        dockerfile: generateDockerfile(serviceList),
-    }
+  return {
+    yaml: await generateYaml(serviceList),
+    dockerfile: generateDockerfile(serviceList),
+  }
 }
