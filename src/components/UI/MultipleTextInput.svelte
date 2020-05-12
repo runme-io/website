4451f7895f12c4ffb6df654446d14ecccc515611
Marlos Borges <marlos.borges@gmail.com>
2020-05-12 15:15:29 +0200

    style: lint files

--- src/components/UI/MultipleTextInput.svelte
+++ src/components/UI/MultipleTextInput.svelte
@@ -1,92 +1,97 @@
 <script>
-    import { createEventDispatcher } from 'svelte'
-    import { faTimes } from '@fortawesome/free-solid-svg-icons'
-    import TextInput from './TextInput.svelte'
-    import ButtonIcon from './ButtonIcon.svelte'
+  import { createEventDispatcher } from 'svelte'
+  import { faTimes } from '@fortawesome/free-solid-svg-icons'
+  import TextInput from './TextInput.svelte'
+  import ButtonIcon from './ButtonIcon.svelte'
 
-    const dispatch = createEventDispatcher()
-    const createInputDescriptor = (value = '', focus = false) => ({ value, focus })
+  const dispatch = createEventDispatcher()
+  const createInputDescriptor = (value = '', focus = false) => ({ value, focus })
 
-    // the label to be shown in the "Add" link
-    export let addLabel
-    export let label
-    export let id
-    export let value
+  // the label to be shown in the "Add" link
+  export let addLabel
+  export let label
+  export let id
+  export let value
 
-    let inputList
+  let inputList
 
-    if (value) {
-        inputList = Array.isArray(value)
-            ? value.map(_value => createInputDescriptor(_value))
-            : [createInputDescriptor(value)]
-    } else {
-        inputList = [createInputDescriptor()]
-    }
+  if (value) {
+    inputList = Array.isArray(value)
+      ? value.map(_value => createInputDescriptor(_value))
+      : [createInputDescriptor(value)]
+  } else {
+    inputList = [createInputDescriptor()]
+  }
 
-    function addInput () {
-        inputList = inputList
-            .map(({ value }) => ({ value, focus: false }))
-            .concat({ value: '', focus: true })
-    }
+  function addInput () {
+    inputList = inputList
+      .map(({ value }) => ({ value, focus: false }))
+      .concat({ value: '', focus: true })
+  }
 
-    function removeInput (inputIndex) {
-        inputList = inputList.filter((_, index) => index !== inputIndex)
-        dispatchChange()
-    }
+  function removeInput (inputIndex) {
+    inputList = inputList.filter((_, index) => index !== inputIndex)
+    dispatchChange()
+  }
 
-    function onInput (input, value) {
-        input.value = value
-        dispatchChange()
-    }
+  function onInput (input, value) {
+    input.value = value
+    dispatchChange()
+  }
 
-    function dispatchChange () {
-        const filteredValues = inputList
-            .map(({ value }) => value.trim())
-            .filter(Boolean)
+  function dispatchChange () {
+    const filteredValues = inputList
+      .map(({ value }) => value.trim())
+      .filter(Boolean)
 
-        const resultValue = filteredValues.length === 1
-            ? filteredValues[0]
-            : filteredValues
+    const resultValue = filteredValues.length === 1
+      ? filteredValues[0]
+      : filteredValues
 
-        dispatch('change', resultValue)
-    }
+    dispatch('change', resultValue)
+  }
 </script>
 
 <style lang="sass">
-    @import './assets/style/theme'
+  @import './assets/style/theme'
 
-    .input-container
-        align-items: center
-        display: flex
+  .input-container
+    align-items: center
+    display: flex
 
-    .button-remove
-        margin-left: $form-control-margin
+  .button-remove
+    margin-left: $form-control-margin
 
 </style>
 
 <div class="form-control">
-    <label for={id}>{label}</label>
+  <label for={id}>{label}</label>
 
-    {#each inputList as input, index}
-        <div class="input-container">
-            <TextInput
-                id={index === 0 ? id : null}
-                compact={true}
-                focus={input.focus}
-                value={input.value}
-                {...$$restProps}
-                on:input={({ target }) => onInput(input, target.value)}
-            />
-            {#if index}
-                <div class="button-remove">
-                    <ButtonIcon
-                        aria-label="Remove"
-                        icon={faTimes}
-                        on:click={() => removeInput(index)}
-                    />
-                </div>
-            {/if}
+  {#each inputList as input, index}
+    <div class="input-container">
+      <TextInput
+        id={index === 0 ? id : null}
+        compact={true}
+        focus={input.focus}
+        value={input.value}
+        {...$$restProps}
+        on:input={({ target }) => onInput(input, target.value)}
+      />
+
+      {#if index}
+        <div class="button-remove">
+          <ButtonIcon
+            aria-label="Remove"
+            icon={faTimes}
+            on:click={() => removeInput(index)}
+          />
         </div>
-    {/each}
-    <a href="/" on:click|preventDefault={addInput}>{addLabel}</a>
+      {/if}
+    </div>
+  {/each}
+
+  <a
+    href="/"
+    on:click|preventDefault={addInput}
+  >{addLabel}</a>
 </div>
--- src/components/UI/TextInput.svelte
+++ src/components/UI/TextInput.svelte
@@ -1,67 +1,79 @@
 <script>
-    import { createEventDispatcher } from 'svelte'
+  import { createEventDispatcher } from 'svelte'
 
-    export let controlType = null
-    export let id = null
-    export let label = null
-    export let rows = null
-    export let value = ''
-    export let min = null
-    export let type = 'text'
-    export let valid = true
-    export let validityMessage = ''
-    export let placeholder = ''
-    // whether this form control has compact style: less margin
-    export let compact = false
-    // whether this element should be focused when rendered
-    export let focus = false
+  export let controlType = null
+  export let id = null
+  export let label = null
+  export let rows = null
+  export let value = ''
+  export let min = null
+  export let type = 'text'
+  export let valid = true
+  export let validityMessage = ''
+  export let placeholder = ''
+  // whether this form control has compact style: less margin
+  export let compact = false
+  // whether this element should be focused when rendered
+  export let focus = false
 
-    const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher()
 
-    let touched = false
+  let touched = false
 
-    function handleEnter (event) {
-      if (event.which === 13) {
-          touched = true
-          dispatch('enter')
-      }
+  function handleEnter (event) {
+    if (event.which === 13) {
+      touched = true
+      dispatch('enter')
     }
+  }
 
-    function init(nodeElement) {
-        if (focus) {
-            nodeElement.focus()
-        }
+  function init (nodeElement) {
+    if (focus) {
+      nodeElement.focus()
     }
+  }
 </script>
 
 <style lang="sass">
-    @import './assets/style/theme'
-    .compact
-        margin-bottom: $label-margin
+  @import './assets/style/theme'
+  .compact
+    margin-bottom: $label-margin
 </style>
 
 <div
-    class="form-control"
-    class:compact={compact}
+  class="form-control"
+  class:compact={compact}
 >
-    {#if label}<label for={id}>{label}</label>{/if}
-    {#if controlType === "textarea"}
-        <textarea class:invalid="{!valid && touched}" {rows} {placeholder} {id} bind:value on:blur={() => touched = true} />
-    {:else}
-        <input
-            class:invalid="{!valid && touched}"
-            {type}
-            {placeholder}
-            {id}
-            {value}
-            {min}
-            use:init
-            on:input
-            on:keydown={handleEnter}
-            on:blur={() => touched = true}
-        >
-    {/if}
-    {#if validityMessage && !valid && touched}
-        <p class="error-message">{validityMessage}</p>
-    {/if}
+  {#if label}<label for={id}>{label}</label>{/if}
+
+  {#if controlType === 'textarea'}
+    <textarea
+      class:invalid="{!valid && touched}"
+      {rows}
+      {placeholder} {id}
+      bind:value
+      on:blur={() => {
+        touched = true
+      }}
+    />
+  {:else}
+    <input
+      class:invalid="{!valid && touched}"
+      {type}
+      {placeholder}
+      {id}
+      {value}
+      {min}
+      use:init
+      on:input
+      on:keydown={handleEnter}
+      on:blur={() => {
+        touched = true
+      }}
+    >
+  {/if}
+
+  {#if validityMessage && !valid && touched}
+    <p class="error-message">{validityMessage}</p>
+  {/if}
 </div>
