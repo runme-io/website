01cbaec3346e6533c66f499a77b79d7f3bbad731
Marlos Borges <marlos.borges@gmail.com>
2020-05-13 09:54:01 +0200

    feat(spec-generator): autoselect created tab (#105)
    * build(svelte-tabs): use forked version with fix
    
    * style: fix linting issues
    
    * feat(spec-generator): autoselect created tab
    
    * fix: review comments

--- src/components/Runme/GenerateSpecTabGroup.svelte
+++ src/components/Runme/GenerateSpecTabGroup.svelte
@@ -1,100 +1,117 @@
 <script>
-    import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs'
-    import { fade } from 'svelte/transition'
-    import { faPlusCircle, faTimes } from '@fortawesome/free-solid-svg-icons'
-    import GenerateSpecForm from './GenerateSpecForm.svelte'
-    import GenerateSpecResult from './GenerateSpecResult.svelte'
-    import Button from '../UI/Button.svelte'
-    import ButtonIcon from '../UI/ButtonIcon.svelte'
-    import Code from '../UI/Code.svelte'
-    import GithubPanel from '../UI/GitHub/GithubPanel.svelte'
-    import specGenerator from '../Stores/SpecGenerator'
-    import { generateSpec } from '../../Helpers'
-    import { DOCKER_SELECT_LANGUAGE, DOCKER_SELECT_SERVICES, ADDITIONAL_SERVICES_LIMIT } from '../../Consts'
+  import { onMount } from 'svelte'
+  import { writable } from 'svelte/store'
+  import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs'
+  import { faPlusCircle, faTimes } from '@fortawesome/free-solid-svg-icons'
+  import GenerateSpecForm from './GenerateSpecForm.svelte'
+  import GenerateSpecResult from './GenerateSpecResult.svelte'
+  import Button from '../UI/Button.svelte'
+  import ButtonIcon from '../UI/ButtonIcon.svelte'
+  import specGenerator from '../Stores/SpecGenerator'
+  import { generateSpec } from '../../Helpers'
+  import { DOCKER_SELECT_LANGUAGE, DOCKER_SELECT_SERVICES, ADDITIONAL_SERVICES_LIMIT } from '../../Consts'
 
-    const services = specGenerator()
+  const services = specGenerator()
 
-    // add app tab
+  // add app tab
+  services.addService()
+
+  const iconRemove = faTimes
+  const iconAdd = faPlusCircle
+  const serviceValidity = {}
+  const selectedTabIndex = writable()
+
+  let spec
+
+  $: filteredServices = $services.slice(1)
+  $: isAddDisabled = $services.length >= ADDITIONAL_SERVICES_LIMIT
+  $: isValid = $services.every((_, index) => serviceValidity[index])
+  $: isGenerateDisabled = !isValid
+
+  function addService () {
     services.addService()
+    selectedTabIndex.set($services.length - 1)
+  }
 
-    const iconRemove = faTimes
-    const iconAdd = faPlusCircle
-    const serviceValidity = {}
+  function removeService (service) {
+    const previousTabIndex = services.removeService(service)
 
-    let spec
+    selectedTabIndex.set(previousTabIndex)
+  }
 
-    $: filteredServices = $services.slice(1)
-    $: isAddDisabled = $services.length >= ADDITIONAL_SERVICES_LIMIT
-    $: isValid = $services.every((_, index) => serviceValidity[index])
-    $: isGenerateDisabled = !isValid
+  async function generate () {
+    spec = await generateSpec($services)
+  }
 
-    async function generate () {
-        spec = await generateSpec($services)
-    }
+  onMount(() => {
+    selectedTabIndex.set($services.length - 1)
+  })
 </script>
 
 <style lang="sass">
-    $tabPadding: .5rem .75rem
+  $tabPadding: .5rem .75rem
 
-    .generate-spec-tab-group
-        display: flex
-        flex-direction: column
+  .generate-spec-tab-group
+      display: flex
+      flex-direction: column
 
-    .generate-spec-tab-group :global(.svelte-tabs li.svelte-tabs__tab)
-        padding: $tabPadding
+  .generate-spec-tab-group :global(.svelte-tabs li.svelte-tabs__tab)
+      padding: $tabPadding
 
-    .tab-add-button
-        display: inline-block
-        vertical-align: middle
+  .tab-add-button
+      display: inline-block
+      vertical-align: middle
 
-        > :global(button)
-            display: flex // TODO: improve css of ButtonIcon
-            padding: $tabPadding !important
+      > :global(button)
+          display: flex // TODO: improve css of ButtonIcon
+          padding: $tabPadding !important
 </style>
 
 <div class="generate-spec-tab-group">
-    <Tabs>
-        <TabList>
-            <Tab>App</Tab>
+  <Tabs
+    selectedTabIndex={$selectedTabIndex}
+  >
+    <TabList>
+      <Tab>App</Tab>
 
-            {#each filteredServices as service, index}
-                <Tab>
-                    Service #{index + 1}
-                    <ButtonIcon
-                        aria-label="Remove Service"
-                        icon={iconRemove}
-                        on:click={() => services.removeService(service)}
-                    />
-                </Tab>
-            {/each}
-            <div class="tab-add-button">
-                <ButtonIcon
-                    flex={true}
-                    aria-label="Add Service"
-                    icon={iconAdd}
-                    disabled={isAddDisabled}
-                    on:click={services.addService}
-                />
-            </div>
-        </TabList>
+      {#each filteredServices as service, index}
+        <Tab>
+          Service #{index + 1}
+          <ButtonIcon
+            aria-label="Remove Service"
+            icon={iconRemove}
+            on:click={() => removeService(service)}
+          />
+        </Tab>
+      {/each}
+      <div class="tab-add-button">
+        <ButtonIcon
+          flex={true}
+          aria-label="Add Service"
+          icon={iconAdd}
+          disabled={isAddDisabled}
+          on:click={addService}
+        />
+      </div>
+    </TabList>
 
-        {#each $services as service, index}
-            <TabPanel>
-                <GenerateSpecForm
-                    sourceType={index === 0 ? DOCKER_SELECT_LANGUAGE : DOCKER_SELECT_SERVICES }
-                    value={service}
-                    on:validate={({ detail }) => serviceValidity[index] = detail}
-                />
-            </TabPanel>
-        {/each}
-    </Tabs>
+    {#each $services as service, index}
+      <TabPanel>
+        <GenerateSpecForm
+          sourceType={index === 0 ? DOCKER_SELECT_LANGUAGE : DOCKER_SELECT_SERVICES }
+          value={service}
+          on:validate={({ detail }) => (serviceValidity[index] = detail)}
+        />
+      </TabPanel>
+    {/each}
+  </Tabs>
 
-    <div class="form-actions">
-        <Button
-            disabled={isGenerateDisabled}
-            on:click={generate}
-        >Generate spec</Button>
-    </div>
+  <div class="form-actions">
+    <Button
+      disabled={isGenerateDisabled}
+      on:click={generate}
+    >Generate spec</Button>
+  </div>
 
-    <GenerateSpecResult {spec} />
+  <GenerateSpecResult {spec} />
 </div>
--- src/components/Stores/SpecGenerator.js
+++ src/components/Stores/SpecGenerator.js
@@ -1,30 +1,37 @@
 import { writable } from 'svelte/store'
 
 const createService = () => ({
-    /*
-    * Whether the user select to provide a image or select one from a list
-    */
-    hasDockerImage: false,
-    dockerImage: '',
-    envVars: {},
-    port: null,
-    build_command: '',
-    command: ''
+  /*
+   * Whether the user select to provide a image or select one from a list
+   */
+  hasDockerImage: false,
+  dockerImage: '',
+  envVars: {},
+  port: null,
+  build_command: '',
+  command: '',
 })
 
 export default function createSpecGenerator () {
-    const { subscribe, update } = writable([])
+  const { subscribe, update } = writable([])
 
-    return {
-      subscribe,
-      addService () {
-          update(services => [
-              ...services,
-              createService(),
-          ])
-      },
-      removeService (serviceToBeRemoved) {
-          update(services => services.filter(service => service !== serviceToBeRemoved))
-      }
-    }
+  return {
+    subscribe,
+    addService () {
+      update(services => [
+        ...services,
+        createService(),
+      ])
+    },
+    removeService (serviceToBeRemoved) {
+      let previousIndex
+
+      update(services => {
+        previousIndex = services.indexOf(serviceToBeRemoved) - 1
+        return services.filter(service => service !== serviceToBeRemoved)
+      })
+
+      return previousIndex
+    },
+  }
 }
