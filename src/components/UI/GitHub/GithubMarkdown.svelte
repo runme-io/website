4a52dff76eb9fc17d708e8b7075f9e7bdafc797b
Marlos Borges <marlos.borges@gmail.com>
2020-05-14 14:06:26 +0200

    feat(generate-form): improve spacing and UI in form (#114)
    * style: lint files
    
    * feat(generate-form): improve spacing and UI in form

--- src/components/UI/GitHub/GithubMarkdown.svelte
+++ src/components/UI/GitHub/GithubMarkdown.svelte
@@ -1,82 +1,89 @@
 <script>
-    import RunmeButton from '../../Runme/RunmeButton.svelte'
-    import GitUrlParse from 'git-url-parse'
-    import { fetchReadme } from '@varandas/fetch-readme'
-    import marked from 'marked'
-    import { application } from '../../Stores/Application'
-    import { onDestroy } from 'svelte'
-    import { setUrl } from '../../../Helpers'
+  import { onDestroy } from 'svelte'
+  import GitUrlParse from 'git-url-parse'
+  import marked from 'marked'
+  import { fetchReadme } from '@varandas/fetch-readme'
+  import RunmeButton from '../../Runme/RunmeButton.svelte'
+  import { application } from '../../Stores/Application'
+  import { setUrl } from '../../../Helpers'
 
-    let content
-    let runUrl
-    const ipsum  = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dignissim pellentesque neque eget gravida.
-    In non lacus in elit aliquam iaculis. Curabitur vel velit efficitur, rhoncus tortor porttitor, dapibus nibh. Fusce efficitur,
-    lorem sit amet sollicitudin blandit, nisl odio tempor ligula, et posuere ante elit ut felis. Morbi felis justo, congue a fringilla vitae,
-    bibendum iaculis nisi. Nullam turpis purus, tempor sit amet eros vel, sagittis accumsan libero. Class aptent taciti sociosqu ad litora torquent
-    per conubia nostra, per inceptos himenaeos. Nunc id luctus nibh. Cras cursus auctor urna, id posuere est blandit id. Nam ut dapibus mauris.
-    Cras mattis venenatis urna. In hac habitasse platea dictumst. Mauris eros mauris, convallis eget malesuada eget, gravida ac massa. Vestibulum
-    luctus ligula libero, sed ornare mi tincidunt a.</p>
+  let content
+  let runUrl
+  const ipsum = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dignissim pellentesque neque eget gravida.
+  In non lacus in elit aliquam iaculis. Curabitur vel velit efficitur, rhoncus tortor porttitor, dapibus nibh. Fusce efficitur,
+  lorem sit amet sollicitudin blandit, nisl odio tempor ligula, et posuere ante elit ut felis. Morbi felis justo, congue a fringilla vitae,
+  bibendum iaculis nisi. Nullam turpis purus, tempor sit amet eros vel, sagittis accumsan libero. Class aptent taciti sociosqu ad litora torquent
+  per conubia nostra, per inceptos himenaeos. Nunc id luctus nibh. Cras cursus auctor urna, id posuere est blandit id. Nam ut dapibus mauris.
+  Cras mattis venenatis urna. In hac habitasse platea dictumst. Mauris eros mauris, convallis eget malesuada eget, gravida ac massa. Vestibulum
+  luctus ligula libero, sed ornare mi tincidunt a.</p>
 
-    <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec est eu quam semper pretium.
-    In sollicitudin augue sit amet viverra elementum. Phasellus ultrices, nisi nec semper auctor, felis orci molestie felis,
-    in bibendum orci sapien eu erat. Sed at lorem ac massa convallis auctor. Aliquam id nunc quis odio rutrum dictum. Suspendisse
-    pellentesque rutrum scelerisque. Curabitur sed ex sit amet massa mollis luctus eu in ex. Vivamus quis malesuada turpis.
-    Maecenas justo massa, iaculis vitae velit sed, venenatis semper odio. Nunc lectus sem, pellentesque vel luctus id, placerat
-    vel urna. Praesent placerat odio in interdum accumsan. Vivamus auctor turpis ac dui dignissim, et maximus ex consectetur.
-    In vitae ornare ipsum, eu luctus turpis.</p>`
+  <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec est eu quam semper pretium.
+  In sollicitudin augue sit amet viverra elementum. Phasellus ultrices, nisi nec semper auctor, felis orci molestie felis,
+  in bibendum orci sapien eu erat. Sed at lorem ac massa convallis auctor. Aliquam id nunc quis odio rutrum dictum. Suspendisse
+  pellentesque rutrum scelerisque. Curabitur sed ex sit amet massa mollis luctus eu in ex. Vivamus quis malesuada turpis.
+  Maecenas justo massa, iaculis vitae velit sed, venenatis semper odio. Nunc lectus sem, pellentesque vel luctus id, placerat
+  vel urna. Praesent placerat odio in interdum accumsan. Vivamus auctor turpis ac dui dignissim, et maximus ex consectetur.
+  In vitae ornare ipsum, eu luctus turpis.</p>`
 
-    const unsubscribe = application.subscribe(({ repo_url, id, repo_name, repo_branch }) => {
-        if (process.browser) {
-            const { owner, name } = GitUrlParse(repo_url)
+  // eslint-disable-next-line camelcase
+  const unsubscribe = application.subscribe(({ repo_url, id, repo_name, repo_branch }) => {
+    if (process.browser) {
+      const { owner, name } = GitUrlParse(repo_url)
 
-            // fetch and parse the markdown
-            fetchReadme({
-                username: owner,
-                repository: name,
-                branch: repo_branch
-            })
-                .then(response => content = marked(response))
-                .catch(() => content = `<h1>${repo_name}</h1><p>${ipsum}</p>`)
+      // fetch and parse the markdown
+      fetchReadme({
+        username: owner,
+        repository: name,
+        branch: repo_branch,
+      })
+        .then(response => (content = marked(response)))
+        // eslint-disable-next-line camelcase
+        .catch(() => (content = `<h1>${repo_name}</h1><p>${ipsum}</p>`))
 
-            // set the run url to build the application
-            runUrl = setUrl(`run?app_id=${id}`)
-        }
-    });
+      // set the run url to build the application
+      runUrl = setUrl(`run?app_id=${id}`)
+    }
+  })
 
-    onDestroy(unsubscribe)
+  onDestroy(unsubscribe)
 </script>
 
-<div class="markdown-body">
-    <p><RunmeButton href={runUrl}/></p>
-    {@html content}
-    <div class="fade"></div>
-</div>
-
 <style lang="scss" global>
-    @import "../../../../node_modules/@primer/css/markdown/index.scss";
+  @import "../../../../node_modules/@primer/css/markdown/index.scss";
 
-    .markdown-body {
-        padding: 4rem;
-        max-height: 80rem;
-        overflow: hidden;
-        position: relative;
+  .markdown-body {
+    max-height: 60rem;
+    overflow: hidden;
+    position: relative;
 
-        table {
-            border-spacing: 0;
-            border-collapse: collapse;
-        }
-
-        .fade {
-            position: absolute;
-            bottom: 0;
-            display: block;
-            width: 100%;
-            height: 20rem;
-
-            background-image: linear-gradient(to bottom,
-                    rgba(255, 255, 255, 0),
-                    rgba(255, 255, 255, 1)
-                    100%);
-        }
+    table {
+      border-spacing: 0;
+      border-collapse: collapse;
     }
+
+    .fade {
+      position: absolute;
+      bottom: 0;
+      display: block;
+      width: 100%;
+      height: 20rem;
+
+      background-image: linear-gradient(
+        to bottom,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 1)
+        100%
+      );
+    }
+  }
 </style>
+
+<div class="markdown-body">
+  <p>
+    <RunmeButton href={runUrl}/>
+  </p>
+
+  {@html content}
+
+  <div class="fade"></div>
+</div>
--- src/components/UI/GitHub/GithubPanel.svelte
+++ src/components/UI/GitHub/GithubPanel.svelte
@@ -1,47 +1,59 @@
 <script>
-    import GitHubBookIcon from './GitHubBookIcon.svelte'
+  import GitHubBookIcon from './GitHubBookIcon.svelte'
 
-    export let displayIcon = true
+  export let displayIcon = true
 
-    $: classes = $$restProps.class || ''
+  $: classes = $$restProps.class || ''
 </script>
 
 <style lang="sass">
-    @import './assets/style/theme'
-    $github-border: .1rem solid #d1d5da
+  @import "./assets/style/theme"
 
-    .github-panel
-        font-family: $font-family-github
-        border: $github-border
-        border-radius: .3rem
-        margin-top: 2rem
+  $github-border: .1rem solid #d1d5da
 
-        .header
-            display: flex
-            flex-direction: row
-            align-items: center
-            padding: .8rem 1.6rem
-            margin: -.1rem -.1rem 0
-            background-color: #f6f8fa
-            border: $github-border
-            border-top-left-radius: .3rem
-            border-top-right-radius: .3rem
-            font-weight: 600
+  .github-panel
+    font-family: $font-family-github
+    border: $github-border
+    border-radius: .3rem
+    margin-top: 2rem
 
-            > div
-                display: flex
-                flex: 1
-                margin-left: 1rem
+    &-header
+      display: flex
+      flex-direction: row
+      align-items: center
+      padding: .8rem 1.6rem
+      margin: -.1rem -.1rem 0
+      background-color: #f6f8fa
+      border: $github-border
+      border-top-left-radius: .3rem
+      border-top-right-radius: .3rem
+      font-weight: 600
+
+      > div
+        display: flex
+        flex: 1
+        margin-left: 1rem
+
+    &-content
+      padding: $spacing
+
+      \:global(pre)
+        padding: 0
+        margin: 0
 </style>
 
 <div class="github-panel {classes}">
-    <div class="header">
-        {#if displayIcon}
-            <GitHubBookIcon/>
-        {/if}
-        <div>
-            <slot name="title" />
-        </div>
+  <div class="github-panel-header">
+    {#if displayIcon}
+      <GitHubBookIcon/>
+    {/if}
+
+    <div>
+      <slot name="title" />
     </div>
+  </div>
+
+  <div class="github-panel-content">
     <slot />
+  </div>
 </div>
