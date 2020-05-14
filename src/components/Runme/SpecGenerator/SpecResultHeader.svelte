9256cdc0adbfa08db5217391717b325ce5575074
Marlos Borges <marlos.borges@gmail.com>
2020-05-14 11:28:36 +0200

    style: lint files

--- src/components/Runme/SpecGenerator/SpecResultHeader.svelte
+++ src/components/Runme/SpecGenerator/SpecResultHeader.svelte
@@ -1,98 +1,98 @@
 <script>
-    import { fade } from 'svelte/transition'
-    import { faCopy, faFileDownload } from '@fortawesome/free-solid-svg-icons'
-    import ButtonIcon from '../../UI/ButtonIcon.svelte'
-    import { DEFAULT_TRANSTION } from '../../../Consts'
+  import { fade } from 'svelte/transition'
+  import { faCopy, faFileDownload } from '@fortawesome/free-solid-svg-icons'
+  import ButtonIcon from '../../UI/ButtonIcon.svelte'
+  import { DEFAULT_TRANSTION } from '../../../Consts'
 
-    const MESSAGE_DELAY = 3000
+  const MESSAGE_DELAY = 3000
 
-    export let content
-    export let filename
-    export let label
+  export let content
+  export let filename
+  export let label
 
-    let linkElement
-    let copied = false
-    let copyMessageTimeout
+  let linkElement
+  let copied = false
+  let copyMessageTimeout
 
-    $: copyLabel = `Copy ${filename} content`
-    $: downloadLabel = `Download ${filename}`
+  $: copyLabel = `Copy ${filename} content`
+  $: downloadLabel = `Download ${filename}`
 
-    function download () {
-        const blob = new Blob([content], { type: 'application/octet-stream' })
+  function download () {
+    const blob = new Blob([content], { type: 'application/octet-stream' })
 
-        linkElement.href = window.URL.createObjectURL(blob)
-        linkElement.target = '_blank'
-        linkElement.download = filename
+    linkElement.href = window.URL.createObjectURL(blob)
+    linkElement.target = '_blank'
+    linkElement.download = filename
 
-        setTimeout(() => window.URL.revokeObjectURL(linkElement.href), 100)
+    setTimeout(() => window.URL.revokeObjectURL(linkElement.href), 100)
+  }
+
+  async function copy () {
+    try {
+      if (copyMessageTimeout) {
+        clearTimeout(copyMessageTimeout)
+      }
+      await navigator.clipboard.writeText(content)
+      copied = true
+      copyMessageTimeout = setTimeout(() => (copied = false), MESSAGE_DELAY)
+    } catch (e) {
+      console.error(e)
     }
-
-    async function copy () {
-        try {
-            if (copyMessageTimeout) {
-                clearTimeout(copyMessageTimeout)
-            }
-            await navigator.clipboard.writeText(content)
-            copied = true
-            copyMessageTimeout = setTimeout(() => copied = false, MESSAGE_DELAY)
-        } catch (e) {
-            console.error(e)
-        }
-    }
+  }
 </script>
 
 <style lang="sass">
-    @import './assets/style/theme'
+  @import "./assets/style/theme"
 
-    .header-title
-        display: flex
-        flex: 1
-        justify-content: space-between
+  .header-title
+    display: flex
+    flex: 1
+    justify-content: space-between
 
-    .download-button
-        text-decoration: none
+  .download-button
+    text-decoration: none
 
-    .button-container
-        display: flex
-        align-items: center
+  .button-container
+    display: flex
+    align-items: center
 
-        \:global(.button-icon)
-            margin-left: 1rem
+    \:global(.button-icon)
+      margin-left: 1rem
 
-        span
-            font-size: 1.2rem
-            color: $success
-            pointer-events: none
+    span
+      font-size: 1.2rem
+      color: $success
+      pointer-events: none
 </style>
 
 <div class="header-title">
-    <a
-        bind:this={linkElement}
-        href="#/"
-        on:click={() => download()}
-        class="download-button"
-        title={downloadLabel}
-    >{label}</a>
+  <a
+    bind:this="{linkElement}"
+    href="#/"
+    on:click="{() => download()}"
+    class="download-button"
+    title="{downloadLabel}"
+  >
+    {label}
+  </a>
 
-    <div class="button-container">
-        {#if copied}
-            <span
-                transition:fade={DEFAULT_TRANSTION}
-            >File content copied!</span>
-        {/if}
-        <ButtonIcon
-            class="button-copy"
-            flex={true}
-            aria-label={copyLabel}
-            icon={faCopy}
-            on:click={copy}
-        />
-        <ButtonIcon
-            class="button-download"
-            flex={true}
-            aria-label={downloadLabel}
-            icon={faFileDownload}
-            on:click={linkElement.click()}
-        />
-    </div>
+  <div class="button-container">
+    {#if copied}
+      <span transition:fade="{DEFAULT_TRANSTION}">File content copied!</span>
+    {/if}
+    <ButtonIcon
+      class="button-copy"
+      flex="{true}"
+      aria-label="{copyLabel}"
+      icon="{faCopy}"
+      on:click="{copy}"
+    />
+    <ButtonIcon
+      class="button-download"
+      flex="{true}"
+      aria-label="{downloadLabel}"
+      icon="{faFileDownload}"
+      on:click="{linkElement.click()}"
+    />
+  </div>
 </div>
