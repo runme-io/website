d3ebc69d5ef05e9b13fa313ece090f6d1da725ec
Reinos <info@reinos.nl>
2020-05-15 13:42:31 +0200

    fix: lint

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,20 +1,28 @@
 <script>
-    import MainLayout from '../components/UI/Layout/MainLayout.svelte'
+  import MainLayout from '../components/UI/Layout/MainLayout.svelte'
 </script>
 
 <MainLayout title="About">
-    <div class="main-content">
-        <h1>Hello!</h1>
+  <div class="main-content">
+    <h1>Hello!</h1>
 
-        <p>Runme is created by <a href="https://jexia.com">Jexia</a> in cooperation with <a href="https://www.digitalocean.com/">Digital Ocean</a> to provide developers a new opportunity and experience to run applications.</p>
+    <p>Runme is created by <a href="https://jexia.com">Jexia</a> in cooperation with
+      <a href="https://www.digitalocean.com/">Digital Ocean</a> to provide developers a new opportunity and
+      experience to run applications.</p>
 
-        <p>As the name already implies: Runme makes it possible to build, deploy and launch an application with just one click.</p>
+    <p>As the name already implies: Runme makes it possible to build, deploy and launch an application with just one
+      click.</p>
 
-        <p>So you can have a running application in a few minutes without the need to setup any environment. You just need to "Runme" and and after few minutes is your application running and can be tested. You can share your running application with clients, friends, colleagues, or even visitors of your Git repo. The application will run for 10 minutes and will be removed afterwards.</p>
+    <p>So you can have a running application in a few minutes without the need to setup any environment. You just need
+      to "Runme" and and after few minutes is your application running and can be tested. You can share your running
+      application with clients, friends, colleagues, or even visitors of your Git repo. The application will run for 10
+      minutes and will be removed afterwards.</p>
 
-        <h2>Contributing</h2>
-        <p>Runme is entirely free to use and is open-source. We ❤ open-source and welcome everyone to contribute whether it's in the form of code, documentation, bug reports, feature requests, or anything else. We encourage you to experiment with the project and make contributions to help evolve it and meet your needs!</p>
+    <h2>Contributing</h2>
+    <p>Runme is entirely free to use and is open-source. We ❤ open-source and welcome everyone to contribute whether
+      it's in the form of code, documentation, bug reports, feature requests, or anything else. We encourage you to
+      experiment with the project and make contributions to help evolve it and meet your needs!</p>
 
-        <p>Thank you for your interest in contributing to Runme!</p>
-    </div>
+    <p>Thank you for your interest in contributing to Runme!</p>
+  </div>
 </MainLayout>
