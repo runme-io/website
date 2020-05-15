d076029c1447bbc96aaa5cb7b4fc0b4a25ac4f1e
Marlos Borges <marlos.borges@gmail.com>
2020-05-15 13:10:07 +0200

    style: lint button-icon

--- src/components/UI/ButtonIcon.svelte
+++ src/components/UI/ButtonIcon.svelte
@@ -1,38 +1,39 @@
 <script>
-    import Icon from 'fa-svelte'
-    import Button from './Button.svelte'
+  import Icon from 'fa-svelte'
+  import Button from './Button.svelte'
 
-    export let icon = null
-    export let label = ''
-    export let mode = 'default'
+  export let icon = null
+  export let label = ''
+  export let mode = 'default'
 
-   $: classes = $$restProps.class || ''
+  $: classes = $$restProps.class || ''
 </script>
 
 <style lang="sass">
-    @import './assets/style/theme'
-    .button-icon-label
-        margin-left: $label-margin
+  @import "./assets/style/theme"
 
-    \:global(.button-icon)
-        display: inline-flex
-        padding-right: 1rem
+  .button-icon-label
+    margin-left: $label-margin
 
-        &:hover
-            \:global(.button-icon-icon)
-                transition: all .25s ease-in-out
-                transform: scale(1.2)
-                color: $gray-medium !important
+  \:global(.button-icon)
+    display: inline-flex
+    padding-right: 1rem
+
+    &:hover
+      \:global(.button-icon-icon)
+        transition: all .25s ease-in-out
+        transform: scale(1.2)
+        color: $gray-medium !important
 </style>
 
 <Button
-    class="button-icon {classes}"
-    {...$$restProps}
-    {mode}
-    on:click
+  class="button-icon {classes}"
+  {...$$restProps}
+  {mode}
+  on:click
 >
-    <Icon class="button-icon-icon" {icon}/>
-    {#if label}
-        <span class="button-icon-label">{label}</span>
-    {/if}
+  <Icon class="button-icon-icon" {icon} />
+  {#if label}
+    <span class="button-icon-label">{label}</span>
+  {/if}
 </Button>
