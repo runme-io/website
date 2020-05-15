fb87d60a61a332f9911262b3c41d280a22d39325
Marlos Borges <marlos.borges@gmail.com>
2020-05-15 12:25:25 +0200

    style: lint alert component

--- src/components/UI/Alert.svelte
+++ src/components/UI/Alert.svelte
@@ -1,56 +1,56 @@
 <script>
-    export let type = ''
+  export let type = ''
 
-    function classType(){
-        switch (type) {
-          case 'success':
-            return 'alert--success'
+  function classType () {
+    switch (type) {
+      case 'success':
+        return 'alert--success'
 
-          case 'danger':
-              return 'alert--danger'
+      case 'danger':
+        return 'alert--danger'
 
-          case 'warning':
-              return 'alert--warning'
+      case 'warning':
+        return 'alert--warning'
 
-          case 'info':
-              return 'alert--info'
-        }
+      case 'info':
+        return 'alert--info'
     }
+  }
 </script>
 
-<div class="alert {classType()}">
-    <slot />
-</div>
-
 <style lang="sass">
-    @import "../../assets/style/theme"
+  @import "./assets/style/theme"
 
-    .alert
-        position: relative
-        padding: 1rem 2rem !important
-        margin-bottom: 3rem
-        border: .1rem solid transparent
-        border-radius: .5rem
-        font-weight: bold
-        font-size: 1.4rem
+  .alert
+    position: relative
+    padding: 1rem 2rem !important
+    margin-bottom: 3rem
+    border: .1rem solid transparent
+    border-radius: .5rem
+    font-weight: bold
+    font-size: 1.4rem
 
-    .alert--success
-        color: $success
-        background-color: #d4edda
-        border-color: #c3e6cb
+  .alert--success
+    color: $success
+    background-color: #d4edda
+    border-color: #c3e6cb
 
-    .alert--danger
-        color: $danger
-        background-color: #f8d7da
-        border-color: #f5c6cb
+  .alert--danger
+    color: $danger
+    background-color: #f8d7da
+    border-color: #f5c6cb
 
-    .alert--warning
-        color: $warning
-        background-color: #fff3cd
-        border-color: #ffeeba
+  .alert--warning
+    color: $warning
+    background-color: #fff3cd
+    border-color: #ffeeba
 
-    .alert--info
-        color: $info
-        background-color: #d1ecf1
-        border-color: #bee5eb
+  .alert--info
+    color: $info
+    background-color: #d1ecf1
+    border-color: #bee5eb
 </style>
+
+<div class="alert {classType()}">
+  <slot />
+</div>
