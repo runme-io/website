b13ff7b52d9fdd93f59cd22251203ac177832ed1
Marlos Borges <marlos.borges@gmail.com>
2020-05-18 15:36:54 +0200

    feat(spec-result): add instructions paragraph

--- src/Consts/SpecGenerator.js
+++ src/Consts/SpecGenerator.js
@@ -1,11 +1,13 @@
 export const ADDITIONAL_SERVICES_LIMIT = 5
 
 export const DOCKER_SELECT_LANGUAGE = {
-    key: 'languages',
-    label: 'What is the language used for this application?',
+  key: 'languages',
+  label: 'What is the language used for this application?',
 }
 
 export const DOCKER_SELECT_SERVICES = {
-    key: 'additional_services',
-    label: `Which docker image does you service need?`,
+  key: 'additional_services',
+  label: 'Which docker image does you service need?',
 }
+
+export const SPEC_URL = 'https://github.com/jexia-com/runme/blob/master/docs/specification.md'
