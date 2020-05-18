0194ae512ded218c2941aaa0650d4ffa6f72b5e3
Marlos Borges <marlos.borges@gmail.com>
2020-05-18 15:31:19 +0200

    refactor: extract generate result to separate component

---
+++ src/components/Runme/GenerateResult.svelte
@@ -0,0 +1,57 @@
+<script>
+  import GenerateSpecResult from './GenerateSpecResult.svelte'
+  import RunmeButton from './RunmeButton.svelte'
+  import ReadmeResult from './ReadmeResult.svelte'
+  import GithubReadme from '../UI/GitHub/GithubReadme.svelte'
+  import { autoscroll } from '../Actions'
+
+  export let scrollOptions = { enabled: true }
+  export let isVisible = true
+  export let spec
+</script>
+
+<style lang="sass">
+  @import "./assets/style/theme"
+  @import "./assets/style/mixins"
+
+  .generate-result
+    @include dashed-line(top)
+    padding-top: $spacing
+    margin-top: $spacing
+
+    > h3
+      font-weight: bold
+      font-size: 2rem
+      margin-bottom: 2rem
+
+  .regular-section
+    margin-top: 2rem
+
+  .dashed-section
+    @include dashed-line(top)
+    margin-top: 3rem
+    padding-top: 3rem
+</style>
+
+{#if isVisible}
+  <div
+    class="generate-result"
+    use:autoscroll={scrollOptions}
+  >
+    <h3>Your <RunmeButton/> button is generated, we're almost there</h3>
+
+    <section class="regular-section">
+      <GenerateSpecResult {spec} />
+    </section>
+
+    <section class="regular-section">
+      <ReadmeResult />
+    </section>
+
+    <section class="dashed-section">
+      <h3>This is how your README.md will look like with <RunmeButton/> button</h3>
+
+      <GithubReadme />
+    </section>
+  </div>
+{/if}
