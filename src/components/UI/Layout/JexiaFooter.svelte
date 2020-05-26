76ea43e4db60e952e1d3d468eb1413de7bdc3fac
Reinos <info@reinos.nl>
2020-05-26 10:03:13 +0200

    refactor: lint

--- src/components/UI/Layout/JexiaFooter.svelte
+++ src/components/UI/Layout/JexiaFooter.svelte
@@ -1,40 +1,43 @@
 <script>
-    import Menu from '../Menu.svelte'
+  import Menu from '../Menu.svelte'
 
-    const date = new Date()
-    const year = date.getFullYear()
+  const date = new Date()
+  const year = date.getFullYear()
 </script>
 
 <footer>
-    <section class="jexia-footer">
-        <Menu />
-        <p>Build for communities that are making impacts with the power of software tools. <br>Runme.io is initiated by <a target="_blank" href="https://jexia.com">Jexia</a> in cooperation with <a target="_blank" href="https://www.digitalocean.com/">Digital Ocean</a>. © Copyright {year}. All rights reserverd.</p>
-        <img src="/images/technology/jexia.svg" alt="Jexia logo"/>
-        <img src="/images/technology/digital-ocean.svg" alt="Digital Ocean logo"/>
-    </section>
+  <section class="jexia-footer">
+    <Menu/>
+    <p>
+      Build for communities that are making impacts with the power of software tools.<br>
+      Runme.io is initiated by <a target="_blank" href="https://jexia.com">Jexia</a> in cooperation with
+      <a target="_blank" href="https://www.digitalocean.com/">Digital Ocean</a>. © Copyright {year}. All rights reserved.</p>
+    <img src="/images/technology/jexia.svg" alt="Jexia logo"/>
+    <img src="/images/technology/digital-ocean.svg" alt="Digital Ocean logo"/>
+  </section>
 </footer>
 
 <style lang="sass">
-    @import '../../../assets/style/theme'
+  @import './assets/style/theme'
 
-    footer
-        margin-top: auto
+  footer
+    margin-top: auto
 
-    .jexia-footer
-        margin-top: calc(#{$spacing} * 2)
-        padding: $spacing
-        background-color: #f4f4f4
-        text-align: center
-        font-family: $font-family-jexia
-        font-size: 1.4rem
+  .jexia-footer
+    margin-top: calc(#{$spacing} * 2)
+    padding: $spacing
+    background-color: #f4f4f4
+    text-align: center
+    font-family: $font-family-jexia
+    font-size: 1.4rem
 
-        p
-            margin-top: 3.5rem
-            color: $gray
+    p
+      margin-top: 3.5rem
+      color: $gray
 
-        img
-            margin-top: 2rem
-            margin-right: 2rem
-            display: inline-block
-            height: 5rem
+    img
+      margin-top: 2rem
+      margin-right: 2rem
+      display: inline-block
+      height: 5rem
 </style>
--- src/components/UI/Layout/MainLayout.svelte
+++ src/components/UI/Layout/MainLayout.svelte
@@ -1,31 +1,31 @@
 <script>
-    import RunmeFooter from './RunmeFooter.svelte'
-    import JexiaFooter from './JexiaFooter.svelte'
-    import Header from './Header.svelte'
-    import TechnologyLogos from './TechnologyLogos.svelte'
+  import RunmeFooter from './RunmeFooter.svelte'
+  import JexiaFooter from './JexiaFooter.svelte'
+  import Header from './Header.svelte'
+  import TechnologyLogos from './TechnologyLogos.svelte'
 
-    export let showRunmeFooter = true
-    export let showTechnologyIcons = false
-    export let title = ''
+  export let showRunmeFooter = true
+  export let showTechnologyIcons = false
+  export let title = ''
 
-    const suffix = 'Runme.io'
-    const pageTitle = title ? `${title} - ${suffix}` : suffix
+  const suffix = 'Runme.io'
+  const pageTitle = title ? `${title} - ${suffix}` : suffix
 </script>
 
 <svelte:head>
-    <title>{pageTitle}</title>
+  <title>{pageTitle}</title>
 </svelte:head>
 
 <div class="page-container">
-    <Header/>
+  <Header/>
 
-    <main>
-        <div class="container">
-            <slot/>
-        </div>
-    </main>
+  <main>
+    <div class="container">
+      <slot/>
+    </div>
+  </main>
 
-    {#if showTechnologyIcons}<TechnologyLogos/>{/if}
-    {#if showRunmeFooter}<RunmeFooter/>{/if}
-    <JexiaFooter/>
+  {#if showTechnologyIcons}<TechnologyLogos/>{/if}
+  {#if showRunmeFooter}<RunmeFooter/>{/if}
+  <JexiaFooter/>
 </div>
