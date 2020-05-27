292349ba6fe40362dc277fbb80e0cd7aea3379e8
Marlos Borges <marlos.borges@gmail.com>
2020-05-27 18:53:25 +0200

    style: lint issues

--- src/components/UI/Code.svelte
+++ src/components/UI/Code.svelte
@@ -1,7 +1,7 @@
 <script context="module">
-    import Highlight from 'svelte-highlight'
-    import { yaml, dockerfile, bash } from 'svelte-highlight/languages'
-    import { githubGist } from 'svelte-highlight/styles'
+  import Highlight from 'svelte-highlight'
+  import { yaml, dockerfile, bash } from 'svelte-highlight/languages'
+  import { githubGist } from 'svelte-highlight/styles'
 </script>
 
 <svelte:head>
@@ -9,11 +9,11 @@
 </svelte:head>
 
 <script>
-    const languages = { yaml, dockerfile, bash }
-    export let lang
-    export let code
+  const languages = { yaml, dockerfile, bash }
+  export let lang
+  export let code
 
-    $: language = languages[lang]
+  $: language = languages[lang]
 </script>
 
 <Highlight {language}>{code}</Highlight>
